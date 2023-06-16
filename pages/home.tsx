b8cb903dde610cad07f44aed5812829fdeeaae38
bjexe <bensimms@knights.ucf.edu>
2023-06-16 13:15:31 -0400

    center tracked accounts from 520px and greater screens

--- pages/home.tsx
+++ pages/home.tsx
@@ -552,7 +552,7 @@ export default function Home() {
               </div>
               {/* view results */}
               <div>
-                <div className="flex flex-col gap-y-8">
+                <div className="flex flex-col gap-y-8 justify-center min-[520px]:items-center">
                   {viewingAll && allTrackedAccountsDisplay}
                   {!viewingAll && bannedTrackedAccountsDisplay}
                 </div>
